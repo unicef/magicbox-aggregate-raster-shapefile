d901c4b5a8f53c69a6f78340214d31c4de92bf7e
mikefab <mikefabrikant@gmail.com>
2018-01-15 14:55:27 -0500

    config sample

---
+++ config-sample.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rasters_dir: './data/rasters/population/worldpop/',
+  shapefiles_dir: './data/shapefiles/gadm2-8/',
+  results_dir: './data/aggregations/population/gadm2-8/'
+}
