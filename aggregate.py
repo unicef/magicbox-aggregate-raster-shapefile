e037e55955142eb21cccc25ce39514673e928c3b
Carlos Cardoso Dias <ccd.leron@gmail.com>
2018-01-16 15:20:11 -0200

    Add mean to output

--- aggregate.py
+++ aggregate.py
@@ -3,6 +3,7 @@ import sys
 from rasterstats import zonal_stats
 
 # shapefile and tif
-stats = zonal_stats(sys.argv[2], sys.argv[1], stats="count sum", geojson_out=True)
+stats = zonal_stats(sys.argv[2], sys.argv[1], stats="count sum mean", geojson_out=True)
 mulu = map(lambda x: (x['properties']),stats)
 print json.dumps(mulu)
+
