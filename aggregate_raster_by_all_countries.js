406338e2e6f36c522ff1b454edd48fb53f74c1a4
Carlos Cardoso Dias <ccd.leron@gmail.com>
2018-01-16 13:38:59 -0200

    Add function to create lookup table
    Lookup table for "country: rasterfile" was being created directly
    from the rasters_dir. Now the function
    create_lookup_country_code_table encapsulates this procedure
    taking into account the parameters passed to the program

--- aggregate_raster_by_all_countries.js
+++ aggregate_raster_by_all_countries.js
@@ -10,17 +10,47 @@ const shapefiles_dir = config.shapefiles_dir
 const aggregate = require('./aggregate_raster_by_shapefile')
 const mkdirp = require('mkdirp');
 
-// Create lookup of country code to raster name
-// Example: arm: ARG_ppp_v2b_2015_UNadj
-const countries = fs.readdirSync(rasters_dir)
-.reduce((h, country) => {
-  if (country.match(/^[a-z]{3}$/)) {
-    h[country] = fs.readdirSync(config.rasters_dir + country).find(file => {
-      return file.match(/tif$/);
-    });
+// Create lookup of country code to rasterfile path 
+// Example: arm: ./data/rasters/arm/ARG_ppp_v2b_2015_UNadj
+const create_lookup_country_code_table = function(single_raster, config) {
+  let table = {}
+  // Default directory to get a list of country codes is 
+  // config.rasters_dir, however, if we have a single raster, we 
+  // have to get all country codes from the shapefiles directory
+  let directory = !!single_raster ? config.shapefiles_dir : config.rasters_dir 
+
+  // Get all subdirectories that matches our pattern for
+  // country_code inside directory
+  let country_codes = fs.readdirSync(directory).filter((element) => {
+    return !!element.match(/^[a-z]{3}$/i)
+  })
+
+  // if we have only one raster
+  if (!!single_raster) {
+    // all country codes will point to it
+    country_codes.reduce((lookup_table, country_code) => {
+      lookup_table[country_code] = single_raster
+      return lookup_table
+    }, table)
+  } else {
+    // we look into each subdirectory of config.rasters_dir
+    // and get the appropriate rasterfile
+    country_codes.reduce((lookup_table, country_code) => {
+      let filename = fs.readdirSync(config.rasters_dir + country_code).find((filename) => {
+        return !!filename.match(/tif$/)
+      })
+
+      lookup_table[country_code] = config.rasters_dir + country_code + '/' + filename 
+
+      return lookup_table
+    })
   }
-  return h;
-}, {})
+
+  return table
+}
+
+// Load lookup table in global variable
+const countries = create_lookup_country_code_table(process.argv[2], config)
 
 // Aggregate a country's raster by each admin level shapefile for that country
 function aggregate_raster_by_all_country_shapefiles(country_code) {
@@ -37,7 +67,7 @@ function aggregate_and_save_results(country_code, shapefile) {
   console.log(country_code, shapefile)
   return new Promise((resolve, reject) => {
     aggregate.aggregate_raster_by_shapefile(
-      rasters_dir + country_code + '/' + countries[country_code],
+      countries[country_code],
       shapefiles_dir + country_code + '/' + shapefile
     ).then(results => {
       mkdirp(results_dir + country_code, (err) => {
